a527aff60f07c28cc0af79837543d31b1c56aec6
Szymon <shateq21@gmail.com>
2022-01-20 12:47:55 +0100

    bStats - if you want to!

--- src/main/java/io/papermc/exampleplugin/ExamplePlugin.java
+++ src/main/java/io/papermc/exampleplugin/ExamplePlugin.java
@@ -2,14 +2,20 @@ package io.papermc.exampleplugin;
 
 import io.papermc.exampleplugin.commands.ExampleCommand;
 import io.papermc.exampleplugin.events.ExampleEvent;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
 
 public final class ExamplePlugin extends JavaPlugin {
+    public static int id = 12345; // Constant plugin id
 
     @Override
     public void onEnable() {
         // What should this plugin do on startup?
+        Metrics m = getMetrics(id);
+
         new ExampleEvent(this);
         PluginCommand hello = getCommand("hello");
         hello.setExecutor(new ExampleCommand());
@@ -20,4 +26,7 @@ public final class ExamplePlugin extends JavaPlugin {
         // Should this plugin do anything when it is disabled?
     }
 
+    private @NotNull Metrics getMetrics(int serviceId) {
+        return new Metrics(this, serviceId);
+    }
 }
